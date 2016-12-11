b12830d16c100f502182314361626ff69d559ae0
oliverakordic <Olivera@Olivera-PC.mshome.net>
2016-12-12 00:19:38 +0100

    Dodata sigurnosna provera u okviru klase NekaDruga.

--- github.com/oliverakordic/Demo.git/Demo/src/NekaDruga.java
+++ github.com/oliverakordic/Demo.git/Demo/src/NekaDruga.java
@@ -3,6 +3,9 @@ import java.util.LinkedList;
 public class NekaDruga {
 	
 	public void prikaziPrvog(LinkedList lista){
+		if(lista==null){
+			System.out.println("Greska!");
+		}
 		System.out.println(lista.get(0));
 	}
 }
