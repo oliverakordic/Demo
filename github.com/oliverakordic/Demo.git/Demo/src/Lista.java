99fef8fb34474fe5a22207324df5071da5d7a3b7
oliverakordic <Olivera@Olivera-PC.mshome.net>
2016-12-11 22:00:40 +0100

    Dodata je klasa Lista sa metodom napraviListu.

---
+++ github.com/oliverakordic/Demo.git/Demo/src/Lista.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class Lista {
+	LinkedList <Student> listaStudenata = new LinkedList<Student>();
+	public void napraviListu(Student s){
+		if(s==null){
+			System.out.println("Greska!");
+			return;
+		}
+		listaStudenata.add(s);
+	}
+}
